4ee4e7df8ce8b2497ce216a2452bbf3c27d2f759
Kenny Allen <105110951+Lallen850@users.noreply.github.com>
2025-04-18 11:52:38 -0500

    Implementing Tailwind

--- learning-app/src/components/Home.jsx
+++ learning-app/src/components/Home.jsx
@@ -2,18 +2,18 @@ import {Playbtn} from './Play-Btn'
 import {MenuBtn} from './Menu-Btn'
 export function Home({onPlayClick,onBackClick,onChange,value}){
 	return(
+//Md implement
 		<>
-			<div className='flex flex-col items-center'>
+			<div className='flex flex-col items-center  '>
 					<MenuBtn onClick={onBackClick}/>
-				<h1>
-					<div className='flex '>
-						<span className='flex m-0.5'><p className='text-red-600'>L</p><p className='text-orange-400'>E</p><p className='text-[#ffff00]'>A</p><p className='text-green-700'>R</p><p className='text-blue-700'>N</p></span>
-						<span className='flex m-0.5'> <p className='text-[#4b0082]'>M </p> <p className='text-[#ee82ee]'>Y</p> </span>
-						<span className='flex m-0.5'><p className='text-red-600'>A</p><p className='text-orange-400'>L</p><p className='text-[#ffff00]'>P</p><p className='text-green-700'>H</p><p className='text-green-700'>A</p><p className='text-blue-700'>B</p><p className='text-[#4b0082]'>E</p><p className='text-[#ee82ee]'>T</p></span>
+				<div className='flex text-6xl font-bold' style={{ fontFamily: '"DynaPuff", system-ui' }}>
+						<span className='flex m-1.5'><p className='text-red-600'>L</p><p className='text-orange-400'>E</p><p className='text-[#ffff00]'>A</p><p className='text-green-700'>R</p><p className='text-blue-700'>N</p></span>
+						<span className='flex m-1.5'> <p className='text-[#4b0082]'>M </p> <p className='text-[#ee82ee]'>Y</p> </span>
+						<span className='flex m-1.5'><p className='text-red-600'>A</p><p className='text-orange-400'>L</p><p className='text-[#ffff00]'>P</p><p className='text-green-700'>H</p><p className='text-green-700'>A</p><p className='text-blue-700'>B</p><p className='text-[#4b0082]'>E</p><p className='text-[#ee82ee]'>T</p></span>
 					</div>
-				</h1>
-					<div className='input'>
+					<div className='flex text-4xl font-bold'>
 						<input 
+						className='flex border-2 border-black-700 place-items-center w-screen'
 						type='text'
 						placeholder="Enter Your Name"
 						value={value.charAt(0).toUpperCase() + value.slice(1)}
@@ -22,33 +22,33 @@ export function Home({onPlayClick,onBackClick,onChange,value}){
 					</div>
 			<Playbtn onPlayClick={onPlayClick}/>
 		</div>
-			<ul className='flex justify-center'>
-					<li className="home-A">A</li>
-					<li className="home-B">B</li>
-					<li className="home-C">C</li>
-					<li className="home-D">D</li>
-					<li className="home-E">E</li>
-					<li className="home-F">F</li>
-					<li className="home-G">G</li>
-					<li className="home-H">H</li>
-					<li className="home-I">I</li>
-					<li className="home-J">J</li>
-					<li className="home-K">K</li>
-					<li className="home-L">L</li>
-					<li className="home-M">M</li>
-					<li className="home-N">N</li>
-					<li className="home-O">O</li>
-					<li className="home-P">P</li>
-					<li className="home-Q">Q</li>
-					<li className="home-R">R</li>
-					<li className="home-S">S</li>
-					<li className="home-T">T</li>
-					<li className="home-U">U</li>
-					<li className="home-V">V</li>
-					<li className="home-W">W</li>
-					<li className="home-X">X</li>
-					<li className="home-Y">Y</li>
-					<li className="home-Z">Z</li>
+			<ul className='flex justify-center text-7xl font-bold' style={{ fontFamily: '"DynaPuff", system-ui' }}>
+					<li className="m-1">A</li>
+					<li  className="m-1">B</li>
+					<li  className="m-1">C</li>
+					<li  className="m-1">D</li>
+					<li  className="m-1">E</li>
+					<li  className="m-1">F</li>
+					<li  className="m-1">G</li>
+					<li  className="m-1">H</li>
+					<li  className="m-1">I</li>
+					<li  className="m-1">J</li>
+					<li  className="m-1">K</li>
+					<li  className="m-1">L</li>
+					<li  className="m-1">M</li>
+					<li  className="m-1">N</li>
+					<li  className="m-1">O</li>
+					<li  className="m-1">P</li>
+					<li  className="m-1">Q</li>
+					<li  className="m-1">R</li>
+					<li  className="m-1">S</li>
+					<li  className="m-1">T</li>
+					<li  className="m-1">U</li>
+					<li  className="m-1">V</li>
+					<li  className="m-1">W</li>
+					<li  className="m-1">X</li>
+					<li  className="m-1">Y</li>
+					<li  className="m-1">Z</li>
 			</ul>
 		</>
 	)
--- learning-app/src/components/Play-Btn.jsx
+++ learning-app/src/components/Play-Btn.jsx
@@ -1,6 +1,6 @@
 
 export function Playbtn({onPlayClick,}){
 	return(
-		<button className='px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded' onClick={onPlayClick}>PLAY</button>
+		<button className='md:text-black-900 md:text-4xl md:font-bold flex md:justify-center justify-center items-center md:h-25 md:w-full w-full  border-t-[0.09em] border-b-[0.09em] border-t-[#f0f0f0] border-b-[#a8a6a6] border-none bg-gradient-to-b from-[#f0f0f0] to-[#a8a6a6] shadow-[0_4px_3px_#a8a6a6] m-2 cursor-pointer ' style={{ fontFamily: '"DynaPuff", system-ui'}} onClick={onPlayClick}>PLAY</button>
 	)
 }
