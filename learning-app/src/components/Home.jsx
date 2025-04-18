2346d8182f3b9ebdef0ae13373516e2144a0679b
Kenny Allen <105110951+Lallen850@users.noreply.github.com>
2025-04-18 00:07:50 -0500

    Implementing Tailwind Home-page

--- learning-app/src/components/Home.jsx
+++ learning-app/src/components/Home.jsx
@@ -3,13 +3,13 @@ import {MenuBtn} from './Menu-Btn'
 export function Home({onPlayClick,onBackClick,onChange,value}){
 	return(
 		<>
-		<div className='home-border'>
+			<div className='flex flex-col items-center'>
 					<MenuBtn onClick={onBackClick}/>
 				<h1>
-					<div className='title'>
-						<span className='title-learn'><p className='text-red-600'>L</p><p className='text-orange-400'>E</p><p className='text-[#ffff00]'>A</p><p className='text-green-700'>R</p><p className='text-blue-700'>N</p></span>
-						<span className='title-my'> <p className='text-[#4b0082]'>M </p> <p className='text-[#ee82ee]'>Y</p> </span>
-						<span className='title-alpha'><p className='text-red-600'>A</p><p className='text-orange-400'>L</p><p className='text-[#ffff00]'>P</p><p className='text-green-700'>H</p><p className='text-green-700'>A</p><p className='text-blue-700'>B</p><p className='text-[#4b0082]'>E</p><p className='text-[#ee82ee]'>T</p></span>
+					<div className='flex '>
+						<span className='flex m-0.5'><p className='text-red-600'>L</p><p className='text-orange-400'>E</p><p className='text-[#ffff00]'>A</p><p className='text-green-700'>R</p><p className='text-blue-700'>N</p></span>
+						<span className='flex m-0.5'> <p className='text-[#4b0082]'>M </p> <p className='text-[#ee82ee]'>Y</p> </span>
+						<span className='flex m-0.5'><p className='text-red-600'>A</p><p className='text-orange-400'>L</p><p className='text-[#ffff00]'>P</p><p className='text-green-700'>H</p><p className='text-green-700'>A</p><p className='text-blue-700'>B</p><p className='text-[#4b0082]'>E</p><p className='text-[#ee82ee]'>T</p></span>
 					</div>
 				</h1>
 					<div className='input'>
@@ -22,7 +22,7 @@ export function Home({onPlayClick,onBackClick,onChange,value}){
 					</div>
 			<Playbtn onPlayClick={onPlayClick}/>
 		</div>
-			<ul className='home-list'>
+			<ul className='flex justify-center'>
 					<li className="home-A">A</li>
 					<li className="home-B">B</li>
 					<li className="home-C">C</li>
--- learning-app/src/components/Menu-Btn.jsx
+++ learning-app/src/components/Menu-Btn.jsx
@@ -1,5 +1,5 @@
 export function MenuBtn({ onClick }){
 	return(
-		<button className='menu-btn' onClick={onClick}>LEARNING MENU</button>
+		<button className='md:text-black md:font-bold font-bold text-black md:text-5xl flex md:justify-center justify-center items-center md:h-40 md:w-full w-full  border-t-[0.09em] border-b-[0.09em] border-t-[#f0f0f0] border-b-[#a8a6a6] border-none bg-gradient-to-b from-[#f0f0f0] to-[#a8a6a6] shadow-[0_4px_3px_#a8a6a6] cursor-pointer hover:bg-linear-[180deg,green_100%] hover:shadow-[0_4px_3px_008000]' style={{ fontFamily: '"DynaPuff", system-ui' }} onClick={onClick}>LEARNING MENU</button>
 	)
 }
