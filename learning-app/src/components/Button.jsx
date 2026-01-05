cc3823a7b2aee5308d901bc1a4fad1d0b8ffafaa
Kenny Allen <105110951+Lallen850@users.noreply.github.com>
2026-01-05 13:43:00 -0600

    Update

--- learning-app/src/components/Button.jsx
+++ learning-app/src/components/Button.jsx
@@ -4,7 +4,3 @@ export function Button({value,onClick,disabled,style,className}){
 		<button className={className} style={style} onClick={onClick} disabled={disabled}>{value}</button>
 	)
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> Updates
